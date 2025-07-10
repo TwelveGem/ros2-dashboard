a248fd3a419dfe6a79d73b7dfe159c4314ef4168
Jon Roth <54535932+TwelveGem@users.noreply.github.com>
2025-07-10 12:06:10 -0400

    Created `Header` to thin `TopicCard`

---
+++ src/components/Header.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  // Props
+  let { isConnected = false } = $props();
+</script>
+
+<div>
+  <!-- BACKGROUND COLOR -->
+  <div class="bg-primary">
+    <!-- SEPARATE ITEMS -->
+    <div class="d-flex justify-content-between px-2 align-items-center">
+      <!-- FAVICON AND TITLE -->
+      <h5 class="mt-2 text-white">
+        <img
+          src="./favicon.png"
+          alt=""
+          style="height: 40px; width: 40px; object-fit: contain;"
+          class="me-2"
+        />
+        ROS Topic Monitor
+      </h5>
+
+      <div class="bg-info">
+        <div class="d-flex align-items-center">
+          <div class="d-flex align-items-center me-3">
+            <div
+              class="badge rounded-pill {isConnected
+                ? 'bg-success'
+                : 'bg-danger'} me-2"
+            >
+              Test
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
